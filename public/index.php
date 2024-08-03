28344af6533c43ee1f9918198ef215200dd2704f
sheanemario <sheanemario7@gmail.com>
2024-08-03 09:15:52 +0530

    initial commit

--- public/index.php
+++ public/index.php
@@ -12,4 +12,15 @@ function splitURL()
     $URL = explode('/', $URL);
     return $URL;
 }
-show(splitURL());
+function loadController()
+{
+    $URL = splitURL();
+
+    $filename = "../app/controllers/" . ucfirst($URL[0]) . ".php";
+    if (file_exists($filename)) {
+        require $filename;
+    } else {
+        echo "controller not found";
+    }
+}
+loadController();
